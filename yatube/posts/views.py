78d347eb3b4cae841dc838b98ec8d82df153012a
Максим Кучишкин <kuchishkin.m@gmail.com>
2022-01-14 17:22:33 +0300

    Homework Django for second review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 from django.http import HttpResponse, HttpRequest
 
+COUNT_OF_POSTS_INDEX = 10
+COUNT_OF_POSTS_GROUP = 10
+
 
 def index(request: HttpRequest) -> HttpResponse:
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:COUNT_OF_POSTS_INDEX]
     context = {
-        'text': 'Последние обновления на сайте.',
+        'text_h1': 'Последние обновления на сайте.',
         'title': 'Главная страница проекта Yatube',
         'posts': posts
     }
@@ -17,11 +20,14 @@ def index(request: HttpRequest) -> HttpResponse:
 def group_posts(request: HttpRequest, slug) -> HttpResponse:
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    text = f'Записи сообщества {group}.'
+    posts = (Post.objects.filter(group=group)[:COUNT_OF_POSTS_GROUP])
+    title = f'Записи сообщества {group.title}.'
     context = {
-        'text': text,
+        'title': title,
+        'text_h1': 'Лев Толстой – зеркало русской революции.',
+        'text_p': 'Группа тайных поклонников графа.',
         'group': group,
         'posts': posts,
     }
+
     return render(request, template, context)
