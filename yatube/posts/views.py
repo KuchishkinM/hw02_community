d80311d602af28a1258df5e946c850588697f1cf
Максим Кучишкин <kuchishkin.m@gmail.com>
2022-01-16 10:03:13 +0300

    Homework Django for third  review.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
 from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
 
-COUNT_OF_POSTS_INDEX = 10
-COUNT_OF_POSTS_GROUP = 10
+from .models import Post, Group
+
+COUNT_OF_POSTS = 10
 
 
 def index(request: HttpRequest) -> HttpResponse:
     template = 'posts/index.html'
-    posts = Post.objects.all()[:COUNT_OF_POSTS_INDEX]
+    posts = Post.objects.select_related('group')[:COUNT_OF_POSTS]
     context = {
         'text_h1': 'Последние обновления на сайте.',
         'title': 'Главная страница проекта Yatube',
@@ -20,14 +20,14 @@ def index(request: HttpRequest) -> HttpResponse:
 def group_posts(request: HttpRequest, slug) -> HttpResponse:
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = (Post.objects.filter(group=group)[:COUNT_OF_POSTS_GROUP])
+    posts = (Post.objects.select_related('group')
+                 .filter(group=group)[:COUNT_OF_POSTS])
     title = f'Записи сообщества {group.title}.'
     context = {
         'title': title,
-        'text_h1': 'Лев Толстой – зеркало русской революции.',
-        'text_p': 'Группа тайных поклонников графа.',
+        'text_h1': f'Посты группы {group.title}',
+        'text_p': f'Группа тайных поклонников графа.',
         'group': group,
         'posts': posts,
     }
-
     return render(request, template, context)
